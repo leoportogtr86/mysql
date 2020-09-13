19626ff24f18bdae44cf7075d5f8e52b9d021b24
leoporto <leonardoreisporto@gmail.com>
2020-09-13 07:23:49 -0300

    criando uma coluna com data e hora no meio dos dados da tabela

--- select_dados_especificos.sql
+++ select_dados_especificos.sql
@@ -3,3 +3,7 @@ select nome, email from pessoas;
 /*poderia ser feita uma busca usando alias (apelidos) tambem*/
 
 select nome as name, idade as age from pessoas;
+
+/*inserindo uma coluna com os dados da data/hora*/
+
+select nome, idade, peso, altura, now() as data_hora from pessoas;
